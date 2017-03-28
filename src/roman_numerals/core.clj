eb0a2c3fed93b6103f0a2729160fa79b40dff141
EA <emashliles@gmail.com>
2017-03-28 14:27:02 +0100

    Attempt 1 - had to use declare and order conditional.

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -1,8 +1,18 @@
 (ns roman-numerals.core)
 
-(defn convert
-  "I convert arabic to roman numerals"
-  [x]
-  (if (= x 0)
-    ""
-    ))
+(declare convert-numeral)
+
+(defn convert [arabic]
+ (convert-numeral arabic "") 
+  )
+
+
+(defn convert-numeral [arabic numeral]
+  (cond (= arabic 0) numeral
+        (<= arabic 3) (convert-numeral (- arabic 1) (str numeral "I"))
+        (= arabic 4) (str numeral "IV")
+        (= arabic 9) (str numeral "IX")
+        (>= arabic 10) (convert-numeral (- arabic 10) (str numeral "X"))
+        (>= arabic 5) (convert-numeral (- arabic 5) (str numeral "V"))
+        ) 
+  )
--- test/roman_numerals/core_test.clj
+++ test/roman_numerals/core_test.clj
@@ -5,3 +5,39 @@
 (deftest converts-0
   (testing "Converts 0"
     (is (= "" (convert 0)))))
+
+(deftest converts-1
+  (testing "Converts 1"
+    (is (= "I" (convert 1)))))
+
+(deftest converts-2
+  (testing "Converts 2"
+    (is (= "II" (convert 2)))))
+
+(deftest converts-3
+  (testing "Converts 3"
+    (is (= "III" (convert 3)))))
+
+(deftest converts-4
+  (testing "Converts 4"
+    (is (= "IV" (convert 4)))))
+
+(deftest converts-5
+  (testing "Converts 5"
+    (is (= "V" (convert 5)))))
+
+(deftest converts-6
+  (testing "Converts 6"
+    (is (= "VI" (convert 6)))))
+
+(deftest converts-9
+  (testing "Converts 9"
+    (is (= "IX" (convert 9)))))
+
+(deftest converts-10
+  (testing "Converts 10"
+    (is (= "X" (convert 10)))))
+
+(deftest converts-14
+  (testing "Converts 14"
+    (is (= "XIV" (convert 14)))))
