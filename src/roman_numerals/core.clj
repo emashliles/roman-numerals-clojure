87709666f8813e73010b83cafd1efc50ee063b26
EA <emashliles@gmail.com>
2017-03-28 15:43:42 +0100

    Attempt-2: better formatting.

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -1,7 +1,12 @@
 (ns roman-numerals.core)
 
-(defn convert
-  "I convert arabic to roman numerals"
-  [x]
-  (if (= x 0)
-    ""))
+(defn to-numerals [arabic numerals] 
+  (cond (= arabic 0) numerals
+        (<= arabic 3) (to-numerals (- arabic 1) (str numerals "I"))
+        (= arabic 4)  (to-numerals (- arabic 4) (str numerals "IV"))
+        (= arabic 9) (to-numerals (- arabic 9) (str numerals "IX"))
+        (>= arabic 10) (to-numerals (- arabic 10) (str numerals "X"))
+        (>= arabic 5) (to-numerals (- arabic 5) (str numerals "V"))))
+
+(defn convert [arabic]
+  (to-numerals arabic ""))
