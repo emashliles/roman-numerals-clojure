1b49c66b3780c1c823d0c6f4c22afb36f9a542dd
Mateu Adsuara <mateuadsuara@gmail.com>
2017-03-29 14:34:36 +0100

    Added support for 6 to 18

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -3,10 +3,10 @@
 (defn convert
   "I convert arabic to roman numerals"
   [arabic]
-  (cond 
-    (= arabic 0) ""
-    (= arabic 1) "I"
-    (= arabic 2) "II"
-    (= arabic 3) "III"
+  (cond
+    (and (>= arabic 0) (<= arabic 3)) (clojure.string/join "" (repeat arabic "I"))
     (= arabic 4) "IV"
-    (= arabic 5) "V"))
+    (and (>= arabic 5) (<= arabic 8)) (str "V" (convert (- arabic 5)))
+    (= arabic 9) "IX"
+    (and (>= arabic 10) (<= arabic 18)) (str "X" (convert (- arabic 10)))
+    ))
--- test/roman_numerals/core_test.clj
+++ test/roman_numerals/core_test.clj
@@ -25,3 +25,55 @@
 (deftest converts-5
   (testing "Converts 5"
     (is (= "V" (convert 5)))))
+
+(deftest converts-6
+  (testing "Converts 6"
+    (is (= "VI" (convert 6)))))
+
+(deftest converts-7
+  (testing "Converts 7"
+    (is (= "VII" (convert 7)))))
+
+(deftest converts-8
+  (testing "Converts 8"
+    (is (= "VIII" (convert 8)))))
+
+(deftest converts-9
+  (testing "Converts 9"
+    (is (= "IX" (convert 9)))))
+
+(deftest converts-10
+  (testing "Converts 10"
+    (is (= "X" (convert 10)))))
+
+(deftest converts-11
+  (testing "Converts 11"
+    (is (= "XI" (convert 11)))))
+
+(deftest converts-12
+  (testing "Converts 12"
+    (is (= "XII" (convert 12)))))
+
+(deftest converts-13
+  (testing "Converts 13"
+    (is (= "XIII" (convert 13)))))
+
+(deftest converts-14
+  (testing "Converts 14"
+    (is (= "XIV" (convert 14)))))
+
+(deftest converts-15
+  (testing "Converts 15"
+    (is (= "XV" (convert 15)))))
+
+(deftest converts-16
+  (testing "Converts 16"
+    (is (= "XVI" (convert 16)))))
+
+(deftest converts-17
+  (testing "Converts 17"
+    (is (= "XVII" (convert 17)))))
+
+(deftest converts-18
+  (testing "Converts 18"
+    (is (= "XVIII" (convert 18)))))
